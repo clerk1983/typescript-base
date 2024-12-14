7a460d23b3f51bfa9c6c3a6acaaf209fac4fe3ce
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-12-14 22:09:07 +0900

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,24 @@
 import js from '@eslint/js'
 import typescriptPlugin from '@typescript-eslint/eslint-plugin'
 import typescriptParser from '@typescript-eslint/parser'
+import jsdoc from 'eslint-plugin-jsdoc'
 import prettier from 'eslint-plugin-prettier'
+import promise from 'eslint-plugin-promise'
 import globals from 'globals'
 
 export default [
+  jsdoc.configs['flat/recommended-typescript'],
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       parser: typescriptParser,
-      sourceType: 'module',
       globals: globals.browser,
     },
     plugins: {
       '@typescript-eslint': typescriptPlugin,
-      prettier: prettier, // Prettier プラグインを追加
+      prettier, // Prettier プラグインを追加
+      jsdoc,
+      promise,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -25,6 +29,14 @@ export default [
         { argsIgnorePattern: '^_' },
       ],
       'no-console': 'warn',
+      'jsdoc/require-description': 'warn',
+      'jsdoc/require-param': 'warn',
+      'jsdoc/require-returns': 'warn',
+      'promise/always-return': 'error',
+      'promise/catch-or-return': 'error',
+      'promise/no-return-wrap': 'warn',
+      'promise/param-names': 'error',
+      'jsdoc/require-description': 'warn',
     },
   },
 ]
