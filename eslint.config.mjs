60a9e8ff9417dd3606adfd6a9a5b645b48512a6c
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-12-14 23:52:14 +0900

    Old style unit test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
-import js from '@eslint/js'
-import typescriptPlugin from '@typescript-eslint/eslint-plugin'
-import typescriptParser from '@typescript-eslint/parser'
-import jsdoc from 'eslint-plugin-jsdoc'
-import prettier from 'eslint-plugin-prettier'
-import promise from 'eslint-plugin-promise'
-import globals from 'globals'
+import js from '@eslint/js';
+import typescriptPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import jest from 'eslint-plugin-jest';
+import jsdoc from 'eslint-plugin-jsdoc';
+import prettier from 'eslint-plugin-prettier';
+import promise from 'eslint-plugin-promise';
+import globals from 'globals';
 
 export default [
   jsdoc.configs['flat/recommended-typescript'],
@@ -12,17 +13,23 @@ export default [
     files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       parser: typescriptParser,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
     },
     plugins: {
       '@typescript-eslint': typescriptPlugin,
       prettier, // Prettier プラグインを追加
       jsdoc,
+      jest,
       promise,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...typescriptPlugin.configs.recommended.rules,
+      ...jest.configs.recommended.rules,
       'prettier/prettier': 'error', // Prettier のルールを適用
       '@typescript-eslint/no-unused-vars': [
         'error',
@@ -36,7 +43,6 @@ export default [
       'promise/catch-or-return': 'error',
       'promise/no-return-wrap': 'warn',
       'promise/param-names': 'error',
-      'jsdoc/require-description': 'warn',
     },
   },
-]
+];
--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,9 @@
-import type { Config } from 'jest'
+import type { Config } from 'jest';
 
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
-}
+};
 
-export default config
+export default config;
