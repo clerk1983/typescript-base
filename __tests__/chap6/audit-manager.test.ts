e0ad27aedd0797a627379cede60b59d08504941a
CLERK1983 <41065074+clerk1983@users.noreply.github.com>
2024-12-15 14:38:25 +0900

    update

--- __tests__/chap6/audit-manager.test.ts
+++ __tests__/chap6/audit-manager.test.ts
@@ -2,7 +2,7 @@ import { AuditManager } from '../../src/chap6/audit-manager';
 
 describe('監査ログ', () => {
   test('追記', () => {
-    const am = new AuditManager(10, './audit');
-    am.addRecord('Akira', new Date());
+    const am = new AuditManager(10, './src/chap6/audits');
+    am.addRecord('Alice', new Date());
   });
 });
